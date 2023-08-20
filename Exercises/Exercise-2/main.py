07ab08d835383201a3ed14239cd844cf356d22c4
Metin Oktay <mettinoktay@gmail.com>
2023-08-20 13:30:48 +0200

    Exercise 2: download the file

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,27 @@
 import requests
 import pandas
+import os
 
+from pathlib import Path
 
+download_url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+download_path = Path(__file__).parent / "downloads"
 def main():
-    # your code here
-    pass
+    print(download_path)
+    
+    try:
+        os.mkdir(download_path)
+        print("downloads folder created.")
+    except FileExistsError:
+        print("Folder already exists. Continuing.")
 
-
+    os.chdir(download_path)
+    input("download about to start")
+    with open("weather_data", "wb") as f:
+        f.write(requests.get(download_url, allow_redirects=True).content)
+    
+    print("Download complete.")
+    
+    
 if __name__ == "__main__":
     main()
